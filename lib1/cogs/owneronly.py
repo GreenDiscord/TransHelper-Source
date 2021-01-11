154281b0fd1dbecf0cbcdd9e8e1ce09ff75c3b50
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-11 23:19:36 +0000

    Add files via upload

---
+++ lib1/cogs/owneronly.py
@@ -0,0 +1,307 @@
+from discord.ext import commands
+import random
+import discord
+import psutil
+import subprocess as sp
+import contextlib
+import inspect
+import time
+import copy
+import os
+import inspect
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
+
+import aiohttp
+import collections
+import ast
+
+def insert_returns(body):
+    # insert return stmt if the last expression is a expression statement
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    # for if statements, we insert returns into the body and the orelse
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
+
+
+
+async def copy_context_with(ctx: commands.Context, *, author=None, channel=None, **kwargs):
+
+    alt_message: discord.Message = copy.copy(ctx.message)
+    alt_message._update(kwargs)  # pylint: disable=protected-access
+
+    if author is not None:
+        alt_message.author = author
+    if channel is not None:
+        alt_message.channel = channel
+
+    # obtain and return a context of the same type
+    return await ctx.bot.get_context(alt_message, cls=type(ctx))
+
+
+class OwnerOnly(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    def owners(ctx):
+      return ctx.author.id == 787800565512929321
+
+    @commands.command(aliases=['s'])
+    @commands.is_owner()
+    async def sync(self, ctx):
+        """Sync with GitHub and reload all the cogs"""
+        embed = discord.Embed(title="Syncing...", description="<a:loading:737722827112972449> Syncing and reloading cogs.")
+        msg = await ctx.send(embed=embed)
+        async with ctx.channel.typing():
+            output = sp.getoutput('git pull')
+        embed = discord.Embed(title="Synced", description="<a:Animated_Checkmark:726140204045303860> Synced with GitHub and reloaded all the cogs.")
+        # Reload Cogs as well
+        error_collection = []
+        for file in os.listdir("cogs"):
+            if file.endswith(".py"):
+                name = file[:-3]
+                try:
+                    self.bot.reload_extension(f"cogs.{name}")
+                except Exception as e:
+                    return await ctx.send(f"```py\n{e}```")
+    @commands.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def dev(self, ctx):
+      await ctx.send("commands for my owner only lol")
+    
+    @dev.group(name='eval')
+    @commands.check(owners)
+    async def _eval(self, ctx, *, body):
+        """Evaluates python code"""
+        env = {
+            'ctx': ctx,
+            'bot': self.bot,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            'source': inspect.getsource
+        }
+
+        def cleanup_code(content):
+            """Automatically removes code blocks from the code."""
+            # remove ```py\n```
+            if content.startswith('```') and content.endswith('```'):
+                return '\n'.join(content.split('\n')[1:-1])
+
+            # remove `foo`
+            return content.strip('` \n')
+
+        env.update(globals())
+
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+        err = out = None
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        def paginate(text: str):
+            '''Simple generator that paginates text.'''
+            last = 0
+            pages = []
+            for curr in range(0, len(text)):
+                if curr % 1980 == 0:
+                    pages.append(text[last:curr])
+                    last = curr
+                    appd_index = curr
+            if appd_index != len(text)-1:
+                pages.append(text[last:curr])
+            return list(filter(lambda a: a != '', pages))
+        
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await ctx.message.add_reaction('\u2049')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    try:
+                        
+                        out = await ctx.send(f'```py\n{value}\n```')
+                    except:
+                        paginated_text = paginate(value)
+                        for page in paginated_text:
+                            if page == paginated_text[-1]:
+                                out = await ctx.send(f'```py\n{page}\n```')
+                                break
+                            await ctx.send(f'```py\n{page}\n```')
+            else:
+                try:
+                    out = await ctx.send(f'```py\n{value}{ret}\n```')
+                except:
+                    paginated_text = paginate(f"{value}{ret}")
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            out = await ctx.send(f'```py\n{page}\n```')
+                            break
+                        await ctx.send(f'```py\n{page}\n```')
+
+        if out:
+            await ctx.message.add_reaction('\u2705')  # tick
+        elif err:
+            await ctx.message.add_reaction('\u2049')  # x
+        else:
+            await ctx.message.add_reaction('\u2705')
+      
+    @dev.group(invoke_without_command=True, name="as")
+    @commands.check(owners)
+    async def dev_as(self, ctx: commands.Context, target: discord.User, *, command_string: str):
+   
+        if ctx.guild:
+            target_member = None
+
+            with contextlib.suppress(discord.HTTPException):
+                target_member = ctx.guild.get_member(target.id) or await ctx.guild.fetch_member(target.id)
+
+            target = target_member or target
+
+        alt_ctx = await copy_context_with(ctx, author=target, content=ctx.prefix + command_string)
+
+        if alt_ctx.command is None:
+            if alt_ctx.invoked_with is None:
+                return await ctx.send('This bot has been hard-configured to ignore this user.')
+            return await ctx.send(f'Command "{alt_ctx.invoked_with}" is not found')
+
+        return await alt_ctx.command.invoke(alt_ctx)
+      
+    @dev.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def chnick(self,ctx,*,name):
+      me3 = ctx.guild.me
+      await me3.edit(nick=name)
+      word1 = "Nickname Changed To"
+      await ctx.send(f"{word1} {name}")
+    
+    
+    
+    
+    @dev.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def changestat(self, ctx):
+      await ctx.send(f"Hi yeah")
+      
+    @changestat.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def stream(self, ctx, *, activity='placeholder (owner to lazy lol)'):
+      await self.bot.change_presence(activity=discord.Streaming(name=activity, url="http://www.twitch.tv/transhelperdiscordbot"))
+      await ctx.send(f'Changed activity to {activity} using Stream status.')
+       
+        
+    @changestat.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def game(self, ctx, *, activity='placeholder (owner to lazy lol)'):
+      await self.bot.change_presence(activity=discord.Game(name=activity))
+      await ctx.send(f'Changed activity to {activity} using Game status.')
+                                     
+    @changestat.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def watching(self, ctx, *, activity='placeholder (owner to lazy lol)'):
+      await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+      await ctx.send(f'Changed activity to {activity} using Watching status.')
+                                     
+    @changestat.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def listening(self, ctx, *, activity='placeholder (owner to lazy lol)'):
+      await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity))
+      await ctx.send(f'Changed activity to {activity} using Listening status.')
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+  
+    
+    @dev.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def stop_bot(self, ctx):
+        emoji = '\N{THUMBS UP SIGN}'
+        message = ctx.message
+        await message.add_reaction(emoji)
+        await ctx.bot.logout()
+
+    @dev.group(invoke_without_command=True)
+    @commands.check(owners)
+    async def send(self, ctx, idd, *, message):
+        lis = ["antastic",
+               "melodic",
+               "quick",
+               "massive",
+               "super",
+               "sophisticated",
+               "unruly",
+               "legal",
+               "elite",
+               "ludicrous",
+               "questionable",
+               "fallacious",
+               "ratty",
+               "chivalrous",
+               "didactic",
+               "sticky",
+               "amuck",
+               "supreme",
+               "polite",
+               "abnormal",
+               "hilarious",
+               "vacuous",
+               "enchanted",
+               "real",
+               "shaky",
+               "infamous",
+               "successful",
+               "royal",
+               "ablaze",
+               "delightful",
+               "miscreant",
+               "flawless",
+               "dynamic",
+               "woebegone",
+               "oopsided",
+               "old",
+               "acid",
+               "vagabond",
+               "level"]
+        channel = self.bot.get_channel(int(idd))
+        await channel.send(f"{message}")
+        await ctx.send(f"Your message {message} was sent!")
+        await channel.send(f"random word of this message = {random.choice(lis)}")
+
+
+def setup(bot):
+    bot.add_cog(OwnerOnly(bot))
---
+++ lib1/cogs/random.py
@@ -0,0 +1,193 @@
+import discord
+from discord.ext import commands
+import datetime
+import random
+from datetime import  timedelta
+from platform import python_version
+from time import time
+import platform
+import asyncio
+from apscheduler.triggers.cron import CronTrigger
+import time
+from discord import Activity, ActivityType, Embed
+from discord import __version__ as discord_version
+from PIL import Image
+import io
+from discord.ext.commands import Cog
+from discord.ext.commands import command
+from psutil import Process, virtual_memory
+from discord.utils import get
+import robloxpy
+import asyncio
+from datetime import timedelta
+from platform import python_version
+from time import time
+import os
+import time
+from gtts import gTTS
+from time import sleep as bedtime
+from apscheduler.triggers.cron import CronTrigger
+from discord import Activity, ActivityType, Embed
+from discord import __version__ as discord_version
+from discord.ext.commands import Cog
+from discord.ext.commands import command
+from psutil import Process, virtual_memory 
+import hypixel
+
+class Random(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.max_concurrency(1, per=commands.BucketType.channel) 
+    @commands.command()
+    async def cookie(self, ctx):
+        """
+        Yum yum.
+        """
+        cookies = ["🍪", "🥠"]
+        reaction = random.choices(cookies, weights=[0.9, 0.1], k=1)[0]
+        embed = discord.Embed(description=f"First one to eat the {reaction} wins!")
+        message = await ctx.send(embed=embed)
+        await asyncio.sleep(4)
+        for i in reversed(range(1, 4)):
+            await message.edit(embed=discord.Embed(description=str(i)))
+            await asyncio.sleep(1)
+        await asyncio.sleep(random.randint(1, 3))
+        await message.edit(embed=discord.Embed(description="Eat the cookie!"))
+        await message.add_reaction(reaction)
+        start = time.perf_counter()
+        try:
+            _, user = await ctx.bot.wait_for(
+                "reaction_add",
+                check=lambda _reaction, user: _reaction.message.guild == ctx.guild
+                and _reaction.message.channel == ctx.message.channel
+                and _reaction.message == message and str(_reaction.emoji) == reaction and user != ctx.bot.user
+                and not user.bot,
+                timeout=60,)
+        except asyncio.TimeoutError:
+            return await message.edit(embed=discord.Embed(description="No one ate the cookie..."))
+        end = time.perf_counter()
+        await message.edit(embed=discord.Embed(description=f"**{user}** ate the cookie in ```{end - start:.3f}``` seconds!"))
+        
+    @command()
+    @commands.cooldown(1, 120, commands.BucketType.guild)
+    async def feedback(self, ctx, *, feed):
+      channel = self.bot.get_channel(794164790368796672)
+      e = discord.Embed(title="Sent Feedback!", description=f"Your feedback '{feed}' has been sent!")
+      await ctx.send(embed=e)
+      await channel.send(f"Feedback from {ctx.author}, Feedback = {feed}")
+    
+    @feedback.error
+    async def support_handler(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            l = self.bot.get_command("support")
+            e = discord.Embed(title=f"Cooldown left - {l.get_cooldown_retry_after(ctx)}", color=discord.colour.Color.from_rgb(231, 84, 128))
+            await ctx.send(embed=e)
+            
+            
+    
+    @command()
+    async def remind(self, ctx, time, *, reminder):
+      e = discord.Embed(title="I will remind you!", descripition=f"I will you remind you in {time} seconds!")
+      await ctx.send(embed=e)
+      await asyncio.sleep(int(time))
+      e2 = discord.Embed(title=f"Hello {ctx.author}", description=f"I have come to remind you to {reminder}!")
+      await ctx.reply(embed=e2)
+      
+    @command(pass_context=True)
+    async def ar(self, ctx, *, role1):
+      member = ctx.message.author
+      role = discord.utils.get(member.guild.roles, name=f"{role1}")
+      await member.add_roles(role)
+      e = discord.Embed(title="Added Roles", description=f"I have added the roles '{role1}' for you!")
+      await ctx.send(embed=e)
+    
+    @command()
+    async def ru(self, ctx, user):
+      if robloxpy.DoesNameExist(user) == False:
+        await ctx.send(f"Member {user} does not exist")
+        pass
+      else:
+        id1 = robloxpy.NameToID(f'{user}')
+        e = discord.Embed(title=f"Stats for {robloxpy.GetName(id1)}", description=f"Are they online? {robloxpy.IsOnline(id1)}", inline=True)
+        e.add_field(name=f"Account Age (in days)? {robloxpy.AccountAgeDays(id1)}", value=f"Year Account Was Created? {robloxpy.UserCreationDate(id1,'Year')}", inline=True)
+        e.add_field(name=f"RAP? {robloxpy.GetUserRAP(id1)}", value=f"Limited Value? {robloxpy.GetUserLimitedValue(id1)}", inline=False)
+        e.set_footer(text=f"Banned User? {robloxpy.IsBanned(id1)}")
+        await ctx.send(embed=e)
+                
+    @command()
+    async def sn(self, ctx, *, name):       
+        tts = gTTS(text=f"Hi! {name} is really cool!", lang='en')
+        tts.save("announce.mp3")
+        await ctx.send(file=discord.File("announce.mp3"))
+        bedtime(5)
+        os.remove("announce.mp3")
+   
+    @command()
+    async def tts(self, ctx, *, text):
+        lol = gTTS(text=f"{text}")
+        lol.save("tts.mp3")
+        await ctx.send(file=discord.File("tts.mp3"))
+        bedtime(5)
+        os.remove("tts.mp3")
+
+    @commands.command(
+        name="stats", description="A usefull command that displays bot statistics."
+    )
+    async def stats(self, ctx):
+        pythonVersion = platform.python_version()
+        dpyVersion = discord.__version__
+        serverCount = len(self.bot.guilds)
+        memberCount = len(set(self.bot.get_all_members()))
+
+        embed = discord.Embed(
+            title=f"{self.bot.user.name} Stats",
+            description="\uFEFF",
+            colour=ctx.author.colour,
+            timestamp=ctx.message.created_at,
+        )
+
+        embed.add_field(name="Bot Version:", value=self.bot.version)
+        embed.add_field(name="Python Version:", value=pythonVersion)
+        embed.add_field(name="Discord.Py Version", value=dpyVersion)
+        embed.add_field(name="Total Guilds:", value=serverCount)
+        embed.add_field(name="Total Users:", value=memberCount)
+        embed.add_field(name="Bot Developers:", value="<@787800565512929321>")
+
+        embed.set_footer(text=f"{ctx.author} | {self.bot.user.name}")
+        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['color', 'colour', 'sc'])
+    async def show_color(self, ctx, *, color: discord.Colour):
+        '''Enter a color and you will see it!'''
+        file = io.BytesIO()
+        Image.new('RGB', (200, 90), color.to_rgb()).save(file, format='PNG')
+        file.seek(0)
+        em = discord.Embed(color=color, title=f'Showing Color: {str(color)}')
+        em.set_image(url='attachment://color.png')
+        await ctx.send(file=discord.File(file, 'color.png'), embed=em)
+        
+    @command()
+    async def hi(self, ctx):
+        await ctx.send("hi.")
+        
+    @command()
+    async def level(self, ctx, name):
+      level = hypixel.get_level(name) 
+      if level is None:
+        e = discord.Embed(title=f"{name}", description=f"Player {name} is not found!, Please make sure to use their **Minecraft** username.")
+        await ctx.send(embed=e)
+      else:
+        e1 = discord.Embed(title=f"Level Of User {name}", description=f"{level}")
+        await ctx.send(embed=e1)
+          
+          
+    @command()
+    async def gay(self, ctx):
+      lis = ["1%", "58%", "32%", "85%", "37%", "48%", "50%"]
+      await ctx.send(f"You are {random.choice(lis)} gay")
+
+def setup(bot):
+    bot.add_cog(Random(bot))
