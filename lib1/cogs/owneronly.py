cf62cf1645f70d03a96e78b5b3adcc5cf0255858
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-12 15:28:03 +0000

    Update owneronly.py

--- lib1/cogs/owneronly.py
+++ lib1/cogs/owneronly.py
@@ -63,7 +63,7 @@ class OwnerOnly(commands.Cog):
         embed = discord.Embed(title="Syncing...", description="<a:loading:737722827112972449> Syncing and reloading cogs.")
         msg = await ctx.send(embed=embed)
         async with ctx.channel.typing():
-            output = sp.getoutput('git pull')
+            output = sp.getoutput('git pull origin main')
         embed = discord.Embed(title="Synced", description="<a:Animated_Checkmark:726140204045303860> Synced with GitHub and reloaded all the cogs.")
         # Reload Cogs as well
         error_collection = []
