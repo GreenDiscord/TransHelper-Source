c79f050c8d1f35be62754fd12187685f96be684c
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-12 16:53:03 +0000

    Update owneronly.py

--- lib1/cogs/owneronly.py
+++ lib1/cogs/owneronly.py
@@ -143,7 +143,7 @@ class OwnerOnly(commands.Cog):
         msg = await ctx.send(embed=embed)
         async with ctx.channel.typing():
             output = sp.getoutput('git pull')
-        embed = discord.Embed(title="Synced", description="<a:Animated_Checkmark:726140204045303860> Synced with GitHub and reloaded all the cogs.", color = discord.colour.Color.green)
+        embed = discord.Embed(title="Synced", description="<a:Animated_Checkmark:726140204045303860> Synced with GitHub and reloaded all the cogs.")
         # Reload Cogs as well
         error_collection = []
         for file in os.listdir("cogs"):
