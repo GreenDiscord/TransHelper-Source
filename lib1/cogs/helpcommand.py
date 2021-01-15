f744f05bccb3abb360aedd0d73439390088047bd
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-15 13:03:00 +0000

    Update helpcommand.py

--- lib1/cogs/helpcommand.py
+++ lib1/cogs/helpcommand.py
@@ -45,7 +45,7 @@ class HelpMenu(ListPageSource):
 		fields = []
 
 		for entry in entries:
-			fields.append((entry.brief or "No description", syntax(entry)))
+			fields.append((entry.brief or "", syntax(entry)))
 
 		return await self.write_page(menu, fields)
 
