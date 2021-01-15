ab0ed110354f3f2398bbd1921617b90695bd6f11
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-15 21:20:16 +0000

    Update moderation.py

--- lib1/cogs/moderation.py
+++ lib1/cogs/moderation.py
@@ -38,8 +38,9 @@ class Moderation(commands.Cog):
         description="A command which kicks a given user",
         usage="<user> [reason]",
     )
+    @commands.bot_has_permissions(kick_members=True)
     @commands.guild_only()
-    @commands.has_guild_permissions(ban_members=True)
+    @commands.has_guild_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         await ctx.guild.kick(user=member, reason=reason)
 
@@ -48,12 +49,21 @@ class Moderation(commands.Cog):
             title=f"{ctx.author.name} kicked: {member.name}", description=reason
         )
         await ctx.send(embed=embed)
+     
+    @kick.error
+    async def error_handler(ctx, error):
+        if isinstance(error, BotMissingPermissions):
+            await ctx.send(f"I need the permissions: {' '.join(error.missing_perms)}")
+                           raise CustomException() from error
+        else:
+                           raise error
 
     @commands.command(
         name="ban",
         description="A command which bans a given user",
         usage="<user> [reason]",
     )
+    @commands.bot_has_permissions()
     @commands.guild_only()
     @commands.has_guild_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
@@ -64,7 +74,14 @@ class Moderation(commands.Cog):
             title=f"{ctx.author.name} banned: {member.name}", description=reason
         )
         await ctx.send(embed=embed)
-
+    
+    @ban.error
+    async def error_handler(ctx, error):
+        if isinstance(error, BotMissingPermissions):
+            await ctx.send(f"I need the permissions: {' '.join(error.missing_perms)}")
+                           raise CustomException() from error
+        else:
+                           raise error
     @commands.command(
         name="unban",
         description="A command which unbans a given user",
