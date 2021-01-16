1820cb417438ea085af54496518f1d35567d168c
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-16 18:38:50 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
 
-async def check(ctx, arg):
+async def check(ctx):
     if isinstance(ctx.channel, discord.channel.DMChannel):
          return ctx.send("Im Sorry, But you can't use commands in DMs! Maybe Go Into A Sever Which Has Me?")
