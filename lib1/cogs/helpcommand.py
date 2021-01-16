e59ecb1b931ad9189ec8cb2ef340ea3bb7eefb6e
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-16 17:30:39 +0000

    Update helpcommand.py

--- lib1/cogs/helpcommand.py
+++ lib1/cogs/helpcommand.py
@@ -7,19 +7,3 @@ class MyNewHelp(commands.MinimalHelpCommand):
         for page in self.paginator.pages:
             embed = discord.Embed(description=page)
             await destination.send(embed=embed)
-
-
-
-class HelpCommand(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.bot.remove_command("help")
-
-    @commands.command()
-    async def help(self, ctx):
-        await ctx.send(embed=MyNewHelp.send_pages)
-
-    
-
-def setup(bot):
-    bot.add_cog(HelpCommand(bot))
