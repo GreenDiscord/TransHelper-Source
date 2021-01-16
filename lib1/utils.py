e38264d2fa450898f6b5577dd17d25e6d6faae72
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-16 18:49:02 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -6,7 +6,6 @@ def check():
     def predicate(ctx):
         if ctx.guild is None:
             raise NoPrivateMessage()
-            await ctx.send("Im Sorry, But you can't use commands in DMs! Maybe Go Into A Sever Which Has Me?")
-        return True
+        return True also await ctx.send("Im Sorry, But you can't use commands in DMs! Maybe Go Into A Sever Which Has Me?")
 
     return check(predicate)
