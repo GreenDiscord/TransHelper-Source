43e87b4ddd2f31a7a1da4f178a2f2bbc543f1320
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-16 17:05:33 +0000

    Create helpcommand.py

---
+++ lib1/cogs/helpcommand.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class MyNewHelp(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            embed = discord.Embed(description=page)
+            await destination.send(embed=embed)
+
+class HelpCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def help(self, ctx):
+        await ctx.send(MyNewHelp.send_pages)
+    
+
+def setup(bot):
+    bot.add_cog(HelpCommand(bot))
