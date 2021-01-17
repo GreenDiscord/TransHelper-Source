5efc790b3037861bbb24c25693d1967de0be853e
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-17 18:55:14 +0000

    Create topgg.py

---
+++ lib1/cogs/topgg.py
@@ -0,0 +1,34 @@
+import dbl
+import discord
+from discord.ext import commands, tasks
+
+import asyncio
+import logging
+
+
+class TopGG(commands.Cog):
+    """Handles interactions with the top.gg API"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.token = bot.token
+        self.dblpy = dbl.DBLClient(self.bot, self.bot.token)
+
+
+    @tasks.loop(minutes=30.0)
+    async def update_stats(self):
+        """This function runs every 30 minutes to automatically update your server count"""
+        logger.info('Attempting to post server count')
+        try:
+            await self.dblpy.post_guild_count()
+            logger.info('Posted server count ({})'.format(self.dblpy.guild_count()))
+        except Exception as e:
+            logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+
+
+        await asyncio.sleep(1800)
+
+def setup(bot):
+    global logger
+    logger = logging.getLogger('bot')
+    bot.add_cog(TopGG(bot))
