160737ff6d754c8917b6df8d7be3e468e40d5782
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-17 20:20:22 +0000

    Update main.py

--- lib1/main.py
+++ lib1/main.py
@@ -29,7 +29,7 @@ intents.guilds = True
 
 
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("th,"), intents=intents, help_command=NewHelp(),  allowed_mentions=discord.AllowedMentions(users=True, roles=True, everyone=False, replied_user=True), case_insenstive=True)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("th,")(bot, message), intents=intents, help_command=NewHelp(),  allowed_mentions=discord.AllowedMentions(users=True, roles=True, everyone=False, replied_user=True), case_insenstive=True)
 bot.db = aiosqlite.connect("main.sqlite")
 bot.version = "15"
 START_BAL = 250
