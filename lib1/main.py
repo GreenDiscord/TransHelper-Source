5cdefe7f6099daa3978c7daa04d1b515cf56bb4d
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-19 19:11:48 +0000

    Update main.py

--- lib1/main.py
+++ lib1/main.py
@@ -82,8 +82,6 @@ async def on_message(message):
     if isinstance(message.channel, discord.DMChannel):
         await message.author.send(':x: Sorry, but I don\'t accept commands through direct messages! Please use the `#bots` channel of your corresponding server!')
         return
-    if bot.dev and not await bot.is_owner(message.author):
-        return
     if bot.user.mentioned_in(message) and message.mention_everyone is False:
         if 'help' in message.content.lower():
             await  message.channel.send('A full list of all commands is available by typing ```th,help```')
