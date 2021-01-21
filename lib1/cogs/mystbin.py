1757695a5dc3d461e6dc6ba13f3968be41549a29
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-21 20:10:19 +0000

    Update mystbin.py

--- lib1/cogs/mystbin.py
+++ lib1/cogs/mystbin.py
@@ -10,18 +10,18 @@ class MystbinApi(commands.Cog):
         self.bot.mystbin_client = bot.mystbin_client
 
 
-        @commands.command()
-        async def mystbin(self, ctx, *, text):
-            paste = await self.bot.mystbin_client.post(f"{text}", syntax="python")
-            e = discord.Embed("I have created a mystbin link for you!", description=f"[Click Here]({paste.url})")
-            await ctx.send(embed=e)
+    @commands.command()
+    async def mystbin(self, ctx, *, text):
+        paste = await self.bot.mystbin_client.post(f"{text}", syntax="python")
+        e = discord.Embed("I have created a mystbin link for you!", description=f"[Click Here]({paste.url})")
+        await ctx.send(embed=e)
 
-        @commands.command()
-        async def getmystbin(self, ctx, id):
-            get_paste = await self.bot.mystbin_client.get(f"https://mystb.in/{id}")
-            lis = ["awesome","bad","good"]
-            e = discord.Embed(title=f"I have found this, is it {random.choice(lis)}?", description=f"The content is shown here:  [Link]({get_paste.url})")
-            await ctx.send(embed=e)
+    @commands.command()
+    async def getmystbin(self, ctx, id):
+        get_paste = await self.bot.mystbin_client.get(f"https://mystb.in/{id}")
+        lis = ["awesome","bad","good"]
+        e = discord.Embed(title=f"I have found this, is it {random.choice(lis)}?", description=f"The content is shown here:  [Link]({get_paste.url})")
+        await ctx.send(embed=e)
 
 
     
