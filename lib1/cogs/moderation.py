9236e0648e2148a4ad6bd31d800b56786fa65b1f
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-21 16:06:07 +0000

    Update moderation.py

--- lib1/cogs/moderation.py
+++ lib1/cogs/moderation.py
@@ -49,7 +49,7 @@ class Moderation(commands.Cog):
           if result_userID == None:
             await ctx.send("This user has no warns")
           else:
-             if member.id == ctx.author.I'd:
+             if member.id == ctx.author.id:
                  name = "You"
                  await cursor.execute(f"SELECT warns FROM warns1 WHERE user_id={USER_ID}")
                  result_User = await cursor.fetchone()
