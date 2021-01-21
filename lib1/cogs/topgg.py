806981b15f2d2ee7178171f1a7cda43afff5c1ca
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-21 07:59:52 +0000

    Update topgg.py

--- lib1/cogs/topgg.py
+++ lib1/cogs/topgg.py
@@ -13,25 +13,12 @@ class TopGG(commands.Cog):
         self.bot = bot
         self.bot.topken = bot.topken
         self.dblpy = dbl.DBLClient(self.bot, self.bot.topken)
-        self.channel = self.bot.get_channel(800450802174263356)
+        self.c = self.bot.get_channel(800450802174263356)
  
     
-    @tasks.loop(minutes=30.0)
-    async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
-        logger.info('Attempting to post server count')
-        await self.channel.send("Starting to attempt server count update!")
-        try:
-            await self.dblpy.post_guild_count()
-            logger.info('Posted server count ({})'.format(self.dblpy.guild_count()))
-            await self.channel.send(f'Updated server count to {len(self.bot.guilds)}')
-        except Exception as e:
-            logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await self.channel.send('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            
-
-
-        await asyncio.sleep(1800)
+    async def on_guild_post():
+        print("Server count posted successfully")
+        await self.c.send(f"Server Count Now Updated!, Current Guilds: {len(self.bot.guilds)}")
 
 def setup(bot):
     global logger
