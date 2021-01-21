dacd33def9e98a7f23c61e86860f15c77234bb05
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-21 20:42:39 +0000

    Update img.py

--- lib1/cogs/img.py
+++ lib1/cogs/img.py
@@ -107,7 +107,7 @@ class ImageManipulation(commands.Cog):
         """Use one of these flags : asexual, bisexual, gay, genderfluid, genderqueer, intersex, lesbian, nonbinary, progress, pan or trans."""
    
         url = str(member.avatar_url_as(format="png", size=1024))
-        img = await self.bot.dagpi.image_process(ImageFeatures.pride(), url=url, flag=flag)
+        img = await self.bot.dagpi.image_process(ImageFeatures.pride(), url=url, flag=f"{flag}")
         e2file = discord.File(fp=img.image,filename=f"pride.{img.format}")
         e = discord.Embed(title="Here You Go! Filter used is pride!")
         e.set_image(url=f"attachment://pride.{img.format}")
