6853cc0dc6887a7f33eeae73832061390c2e48a6
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-22 10:31:50 +0000

    Update music.py

--- lib1/cogs/music.py
+++ lib1/cogs/music.py
@@ -1,210 +1,56 @@
-from discord.ext import commands
-import lavalink
-from discord import utils
-from discord import Embed
 import discord
-import math
-import re
+import wavelink
+from discord.ext import commands
 
-class MusicCog(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
-    self.bot.music = lavalink.Client(self.bot.user.id)
-    self.bot.music.add_node('lavalink.something.host', 80, 'youshallnotpass', 'na', 'music-node')
-    self.bot.add_listener(self.bot.music.voice_update_handler, 'on_socket_response')
-    self.bot.music.add_event_hook(self.track_hook)
-  
-  
-  
-  @commands.command(name='join')
-  async def join(self, ctx):
-    print('join command worked')
-    member = utils.find(lambda m: m.id == ctx.author.id, ctx.guild.members)
-    if member is not None and member.voice is not None:
-      vc = member.voice.channel
-      player = self.bot.music.player_manager.create(ctx.guild.id, endpoint=str(ctx.guild.region))
-      if not player.is_connected:
-        player.store('channel', ctx.channel.id)
-        await self.connect_to(ctx.guild.id, str(vc.id))
-        e = discord.Embed(title="Joined Voice Channel", description=f"Joined Voice Channel {vc}")
-        await ctx.send(embed=e)
-      else:
-        await ctx.send("I'm already connected!")
-  
-  
-  @commands.command(name='skip', aliases=['s'])
-  @commands.guild_only()
-  async def _skip(self, ctx):
-      """ Skips the current track. """
-      player = self.bot.music.player_manager.get(ctx.guild.id)
 
-      if not player.is_playing:
-          return await ctx.send('Not playing.')
 
-      await player.skip()
-      await ctx.send('⏭ | Skipped.')
-      
-    
-  @commands.command(name="queue", aliases=["q"])
-  async def queue(self, ctx):
-    """Display queue [q]"""
-    player = self.bot.music.player_manager.get(ctx.guild.id)
-    
-    
-    desc = ""
-	
-    if (len(player.queue) == 0 and not(player.current)):
-      desc = "Empty Queue"
-      await ctx.send(f"{desc}")
-    else:
-      desc = "1. " + player.current.title
-      for i in range(0, len(player.queue)):
-        desc = desc + '\n' + str(i+2) + ". " + player.queue[i].title
-        e = discord.Embed(title="Queue", description=f"{desc}")
-        await ctx.send(embed=e)
-        
-  @commands.command(name='now', aliases=['np', 'n', 'playing'])
-  @commands.guild_only()
-  async def _now(self, ctx):
-        """ Shows some stats about the currently playing song. """
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        song = 'Nothing'
 
-        if player.current:
-            position = lavalink.Utils.format_time(player.position)
-            if player.current.stream:
-                duration = '🔴 LIVE'
-            else:
-                duration = lavalink.Utils.format_time(player.current.duration)
-            song = f'**[{player.current.title}]({player.current.uri})**\n({position}/{duration})'
 
-        embed = discord.Embed(color=discord.Color.blurple(),
-                              title='Now Playing', description=song)
-        await ctx.send(embed=embed)     
-  @commands.command(name="remove", aliases=["r"])
-  async def remove(self, ctx, idx):
-    """[idx] - Remove song with given index from queue [r]"""
-    player = self.bot.music.player_manager.get(ctx.guild.id)
-    
-    try:
-      idx = int(idx) 
-    
-    except ValueError:
-      e1 = discord.Embed(title="Enter index of song", color = discord.Color.red())
-      return await ctx.send(embed=e1)
-      
-    if ((idx < 1) or (idx > len(player.queue)+1)):
-      e2 = discord.Embed(title="Enter proper index", color=discord.Color.red())
-      return await ctx.send(embed=e2)
-        
-    if not player.current:
-      e3 = discord.Embed(title="Empty queue", color=discord.Color.red())
-      return await ctx.send(embed=e3)
-        
-    if (idx == 1):
-      removed = player.current.title
-      await player.skip()
+class Music(commands.Cog):
 
-		
-    else:
-      removed = player.queue[idx-2].title
-      del player.queue[idx-2]
-      e4 = discord.Embed(title=remove + " removed", color = discord.Color.green())
-      await ctx.send(embed=e4)
-  
-        
-  @commands.command(name='leave')
-  async def leavevoice(self,ctx):
-    player = self.bot.music.player_manager.get(ctx.guild.id)
-    await self.connect_to(ctx.guild.id, None)
-    e = discord.Embed(title="Left Channel", description="Left Voice Channel!")
-    await ctx.send(embed=e)
-    player.queue.clear()
-    
-  @commands.command(name='pause', aliases=["ts"])
-  async def commanda(self, ctx):
-    player = self.bot.music.player_manager.get(ctx.guild.id)
-    if player.is_playing:
-      await player.set_pause(True)
-      e = discord.Embed(title="Paused", description=f"Paused The Current Song")
-      await ctx.send(embed=e)
-    else:
-      await ctx.send("Im not in a channel dummy")
-  
-  @commands.command(name='unpause', aliases=["up"])
-  async def commandb(self, ctx):
-    player = self.bot.music.player_manager.get(ctx.guild.id)
-    await player.set_pause(False)
-    e = discord.Embed(title="Unpaused", description="Unpaused The Current Song")
-    await ctx.send(embed=e)
- 
-  @commands.command(name='volume', aliases=['v'])
-  @commands.guild_only()
-  async def _volume(self, ctx, volume: int = None):
-    """ Changes the player's volume. Must be between 0 and 1000. Error Handling for that is done by Lavalink. """
-    player = self.bot.music.player_manager.get(ctx.guild.id)
-    
-    if not volume:
-      return await ctx.send(f'🔈 | {player.volume}%')
+    def __init__(self, bot):
+        self.bot = bot
 
-    await player.set_volume(volume)
-    await ctx.send(f'🔈 | Set to {player.volume}%')
-  
-  @commands.command(name='stop')
-  async def commandc(self, ctx):
-    player = self.bot.music.player_manager.get(ctx.guild.id)
-    
-    if player.is_playing:
-      e = discord.Embed(title="Stopped", description=f"Stopped Playing and Left Channel")
-      await ctx.send(embed=e)
-      await player.stop()
-      await self.connect_to(ctx.guild.id, None)
-      
-    else:
-      await ctx.send("Im not playing anything?")
-      
-  
-    
-  @commands.command(name='play')
-  async def play(self, ctx, *, query):
-    try:
-      player = self.bot.music.player_manager.get(ctx.guild.id)
-      query2 = f'ytsearch:{query}'
-      results = await player.node.get_tracks(query2)
-      tracks = results['tracks'][0:10]
-      i = 0
-      query_result = ''
-      for track in tracks:
-        i = i + 1
-        query_result = query_result + f'{i}) {track["info"]["title"]} - {track["info"]["uri"]}\n'
-      embed = Embed()
-      embed.description = query_result
+        if not hasattr(bot, 'wavelink'):
+            self.bot.wavelink = wavelink.Client(bot=self.bot)
 
-      await ctx.channel.send(embed=embed)
+        self.bot.loop.create_task(self.start_nodes())
 
-      def check(m):
-        return m.author.id == ctx.author.id
-      
-      response = await self.bot.wait_for('message', check=check)
-      track = tracks[int(response.content)-1]
+    async def start_nodes(self):
+        await self.bot.wait_until_ready()
 
-      player.add(requester=ctx.author.id, track=track)
-      if not player.is_playing:
-        await player.play()
-        await ctx.send(f"Playing {query}")
+        # Initiate our nodes. For this example we will use one server.
+        # Region should be a discord.py guild.region e.g sydney or us_central (Though this is not technically required)
+        await self.bot.wavelink.initiate_node(host='0.0.0.0',
+                                              port=2333,
+                                              rest_uri='http://0.0.0.0:2333',
+                                              password='youshallnotpass',
+                                              identifier='TEST',
+                                              region='us_central')
 
-    except Exception as error:
-      print(error)
-  
-  
-  async def track_hook(self, event):
-    if isinstance(event, lavalink.events.QueueEndEvent):
-      guild_id = int(event.player.guild_id)
-      await self.connect_to(guild_id, None)
-      
-  async def connect_to(self, guild_id: int, channel_id: str):
-    ws = self.bot._connection._get_websocket(guild_id)
-    await ws.voice_state(str(guild_id), channel_id)
-    
-def setup(bot):
-  bot.add_cog(MusicCog(bot))
+    @commands.command(name='connect')
+    async def connect_(self, ctx, *, channel: discord.VoiceChannel = None):
+        if not channel:
+            try:
+                channel = ctx.author.voice.channel
+            except AttributeError:
+                raise discord.DiscordException('No channel to join. Please either specify a valid channel or join one.')
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        await ctx.send(f'Connecting to **`{channel.name}`**')
+        await player.connect(channel.id)
+
+    @commands.command()
+    async def play(self, ctx, *, query: str):
+        tracks = await self.bot.wavelink.get_tracks(f'ytsearch:{query}')
+
+        if not tracks:
+            return await ctx.send('Could not find any songs with that query.')
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        if not player.is_connected:
+            await ctx.invoke(self.connect_)
+
+        await ctx.send(f'Added {str(tracks[0])} to the queue.')
+        await player.play(tracks[0])
+def setup
