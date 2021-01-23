2748ce2b2c277fa0960b755f252438bc228d17fe
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-23 10:38:35 +0000

    Update reactions.py

--- lib1/cogs/reactions.py
+++ lib1/cogs/reactions.py
@@ -15,34 +15,7 @@ class Reactions(commands.Cog):
         self.cur = self.db.cursor()
 
   
-
-    @Cog.listener()
-    async def on_raw_reaction_add(self, payload):
-            cursor = self.bot.db.cursor()
-            if payload.emoji.name == "⭐":
-                message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
-
-                if not payload.member.id == message.author.id:
-			embed = discord.Embed(title="Starred message")
-
-		        fields = [("Author", message.author.mention, False), ("Content", message.content or "See attachment", False), ("Stars", stars+1, False)]
-
-		        for name, value, inline in fields:
-				embed.add_field(name=name, value=value, inline=inline)
-
-	                if len(message.attachments):
-				embed.set_image(url=message.attachments[0].url)
-
-		        if not stars:
-				star_message = await self.starboard_channel.send(embed=embed)
-				
-		        else:
-				star_message = await self.starboard_channel.fetch_message(msg_id)
-				await star_message.edit(embed=embed)
-				
-
-                else:
-                    await message.remove_reaction(payload.emoji, payload.member)
+#breaking bot
 
     
 
