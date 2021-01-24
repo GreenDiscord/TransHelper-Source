5df55636ed52e4f10fef0e5949076839fce4e716
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-24 22:10:10 +0000

    Update game.py

--- lib1/cogs/game.py
+++ lib1/cogs/game.py
@@ -1,4 +1,4 @@
-import discord, datetime, time
+import discord, datetime, time, random, asyncio
 from discord.ext import commands
 
 
@@ -31,6 +31,46 @@ class Games(commands.Cog):
                         response = await self.bot.chatbot.ask(m.content) # Ask a question, returns async_cleverbot.cleverbot.Response
                         await ctx.send(response.text)
     
+    @commands.max_concurrency(1, per=commands.BucketType.channel) 
+    @commands.command()
+    async def reaction(self, ctx):
+        """
+        Yum Yum or Yuck Yuck?
+        """
+        cookies = ["🍪", "❤"]
+        lis = ["this mighty","this weak","this amazing"]
+        reaction = random.choices(cookies, weights=[0.9, 0.1], k=1)[0]
+        embed = discord.Embed(description=f"So, {random.choice(lis)} fighter has challenged people to a game of....Cookie? Okay then get ready!")
+        message = await ctx.send(embed=embed)
+        await asyncio.sleep(4)
+        for i in reversed(range(1, 4)):
+            await message.edit(embed=discord.Embed(description=str(i)))
+            await asyncio.sleep(1)
+        await asyncio.sleep(random.randint(1, 3))
+        await message.edit(embed=discord.Embed(description=f"React to the {reaction}!"))
+        await message.add_reaction(reaction)
+        start = time.perf_counter()
+        try:
+            _, user = await ctx.bot.wait_for(
+                "reaction_add",
+                 check=lambda _reaction, user: _reaction.message.guild == ctx.guild
+                 and _reaction.message.channel == ctx.message.channel
+                 and _reaction.message == message and str(_reaction.emoji) == reaction and user != ctx.bot.user
+                 and not user.bot,
+                 timeout=60,)
+            if not user.id == 787800565512929321:
+               return await ctx.send("Hahaha, Your not wining, only Green can :)")
+        except asyncio.TimeoutError:
+            return await message.edit(embed=discord.Embed(description="No one ate the cookie..."))
+        end = time.perf_counter()
+        await message.edit(embed=discord.Embed(description=f"**{user}**  ate the cookie in ```{end - start:.3f}``` seconds!"))
+        lis3 = ["1", "2"]
+        choice = random.choice(lis3)
+        if choice == 2:
+             await user.send(f"Firstly, Random chose 2 so you get this DM, Secondly, Well Done! You completed it in ```{end - start:.3f}``` seconds.")
+        else:
+             pass
+
     @commands.max_concurrency(1, per=BucketType.guild, wait=False)
     @commands.command(aliases=['2048', '24'])
     async def twenty(self, ctx):
@@ -44,7 +84,7 @@ class Games(commands.Cog):
         ]
         score = 0
         total = 0
-        embed=discord.Embed(title="2048", description=f"If a reaction is not received every 2 minutes, the game will time out.\n\n```{self.print_board(board)}```", color=self.bot.color)
+        embed=discord.Embed(title="2048", description=f"If a reaction is not received every 2 minutes, the game will time out.\n\n```{self.print_board(board)}```")
         message = await ctx.send(embed=embed)
         await message.add_reaction("\u2B06")
         await message.add_reaction("\u2B07")
@@ -93,7 +133,7 @@ class Games(commands.Cog):
                     await message.delete()
                     return
                 board = nb
-                sem=discord.Embed(title=f"Score: **{score}**", description=f"```{self.print_board(board)}```", color=self.bot.color)
+                sem=discord.Embed(title=f"Score: **{score}**", description=f"```{self.print_board(board)}```")
                 'await message.edit(content=f"Score: **{score}**```{self.print_board(board)}```")'
                 await message.edit(embed=sem)
 
