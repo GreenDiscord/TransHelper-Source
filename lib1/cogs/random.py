17ee6fca867e1c52e1273d2b1cc45738c804cb42
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-26 21:56:24 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -147,10 +147,14 @@ class Random(commands.Cog):
         
     @command()
     async def level(self, ctx, name):
-                        hypixel = await Hypixel(self.API_KEY)
-                        player = await hypixel.player.get(name=f"{name}")
-                        e2 = discord.Embed(title=f"Level For Player {player.name}",description=f"{player.level.level}")
-                        await ctx.send(embed=e2)
+                        hypixel = await Hypixel(self.API_KEY)    
+                        try:
+                            player = await hypixel.player.get(name=f"{name}")
+                            e2 = discord.Embed(title=f"Level For Player {player.name}",description=f"{player.level.level}")
+                            await ctx.send(embed=e2)
+                        except Hypixel.NoPlayerFoundError:
+                            e = discord.Embed(title="Not Found!", description=f"Player {name} was not found, remember to use their/your **Minecraft** User Name")
+                            await ctx.send(embed=e)
       
     @command()
     async def gay(self, ctx):
