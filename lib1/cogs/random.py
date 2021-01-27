97dfda8bd0e56217e87c309c3f15fc6326780928
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-27 00:43:02 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -78,9 +78,10 @@ class Random(commands.Cog):
     
     @command(usage="ru <user>")
     async def ru(self, ctx, id):
-        user = await self.roblox.get_user_info(int(id))
+        id = int(id)
+        user = await self.roblox.get_user_info(id)
         e = discord.Embed(title=f"Stats For {user.name}", description=f"Ammount Of Friends? {await user.friends()}", color = discord.Color.red())
-        e.set_image(url=user.thumbnail)
+        e.set_image(url=f"https://www.roblox.com/headshot-thumbnail/json?userId={id}&width=180&height=180")
         await ctx.send(embed=e)
 
     @command(usage="sn <name>")
