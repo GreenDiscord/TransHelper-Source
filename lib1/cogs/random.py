c8c8cca0142245c8088b75922755d02dd739d4f6
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-27 10:43:03 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -83,8 +83,7 @@ class Random(commands.Cog):
                 msg = await ctx.send("Getting Info Now!")
                 user = await self.roblox.get_user_by_name(name)
                 id = int(user.id) 
-                channel = await user.promotion_channel()
-                e = discord.Embed(title=f"ID? {user.id}", description=f"Promotion Channel? {channel}", color = discord.Color.red())
+                e = discord.Embed(title=f"ID? {user.id}", description=f"Oldest Friend? {user.oldest_friend}", color = discord.Color.red())
                 description = user.description
                 if description is None:
                    description = "None"
