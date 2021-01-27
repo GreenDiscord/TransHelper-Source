1d16308fb9ec06fbad1abb900f00ac0cb6b93186
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-27 01:58:37 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -82,8 +82,9 @@ class Random(commands.Cog):
         id = int(user.id)
         e = discord.Embed(title=f"Stats For {user.name}", color = discord.Color.red())
         e.add_field(name=f"Amount Of Friends? {len(await user.friends())}", value = f"Amount Of Followers? {await user.following_count()}")
-        e.add_field(name=f"Account Creation Time? {user.created_at}", value = f"Status? {user.primary_group}")
-        e.set_thumbnail(url=f"https://www.roblox.com/headshot-thumbnail/image?userId={id}&width=150&height=150&format=png")
+        e.add_field(name=f"Account Creation Time? {user.created_at}", value = None)
+        e.set_author(url=f"https://www.roblox.com/headshot-thumbnail/image?userId={id}&width=150&height=150&format=png")
+        e.set_thumbnail(url=f"https://www.roblox.com/outfit-thumbnail/image?userOutfitId={id}&width=420&height=420&format=png")
         await ctx.send(embed=e)
 
     @command(usage="sn <name>")
