a2e1c4bf1fbfbcb2f4853322dbe35b7ddee0a6e9
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-27 10:33:26 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -83,9 +83,7 @@ class Random(commands.Cog):
                 msg = await ctx.send("Getting Info Now!")
                 user = await self.roblox.get_user_by_name(name)
                 id = int(user.id) 
-                gameid = await user.latest_public_game().id
-                game = self.roblox.get_place_by_id(gameid)
-                e = discord.Embed(title=f"ID? {user.id}", description=f"Newest Game Visits? {game.visits}", color = discord.Color.red())
+                e = discord.Embed(title=f"ID? {user.id}", description=f"Newest Game ? {user.latest_public_game()}", color = discord.Color.red())
                 description = user.description
                 if description is None:
                    description = "None"
