cddaf995097b5990a96381c354a04c7a5d15c0b3
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-27 09:04:11 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -83,6 +83,7 @@ class Random(commands.Cog):
             msg = await ctx.send("Getting Info Now!")
             user = await self.roblox.get_user_by_name(name)
             id = int(user.id)
+            age = await user.account_age()
             e = discord.Embed(title=f"Stats For {user.name}", color = discord.Color.red())
             description = user.description
             lendec = len(description)
@@ -92,7 +93,7 @@ class Random(commands.Cog):
             if lendec > 40:
                 description = "I can't send this, it's to big!"
             e.add_field(name=f"Amount Of Friends? {len(await user.friends())}", value = f"Amount Of Followers? {await user.following_count()}")
-            e.add_field(name=f"Account Creation Time? {user.created_at}", value = f"Description? {description}")
+            e.add_field(name=f"Account Creation Time? {age}", value = f"Description? {description}")
             e.add_field(name=f"Number Of Games? {gamecount}", value=f"[Direct Link](https://www.roblox.com/users/{id}/profile)")
             e.set_author(name=f"{ctx.author}", icon_url=f"https://www.roblox.com/headshot-thumbnail/image?userId={id}&width=150&height=150&format=png")
             e.set_thumbnail(url=f"{avatar}")
