8f9659c442afec3a82ae2cb9eaebf918d68d4817
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-28 13:37:59 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -95,7 +95,8 @@ class Random(commands.Cog):
                 gamecount = len(games)
                 if lendec > 1080:
                     description = "I can't send this, it's to big!"
-                if user.is_premium is True:
+                isprem = await user.is_premium()
+                if isprem is True:
                     e.add_field(name=f"Trade Link?", value=f"[Click Here!](https://www.roblox.com/users/{id}/trade)")
                 e.add_field(name=f"Amount Of Friends? {len(await user.friends())}", value = f"Amount Of Followers? {await user.following_count()}")
                 e.add_field(name=f"Account Age? {user.account_age().years} Years, {user.account_age().months} Months, {user.account_age().days} Days", value = f"Description? {description}")
