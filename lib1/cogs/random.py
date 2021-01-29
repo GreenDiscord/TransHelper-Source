f0b17f4b5aa9334a8ea285f4a041a5f3caa5d6c9
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-01-29 22:57:19 +0000

    Update random.py

--- lib1/cogs/random.py
+++ lib1/cogs/random.py
@@ -67,7 +67,7 @@ class Random(commands.Cog):
     @command()
     async def qr(self, ctx, url, colour="0-0-0"):
         colours = dict([("red", "FF0000"), ("blue", "00f")])
-        col = ["black", "red", "blue")
+        col = ["black", "red", "blue"]
 
         if colour in col:
            yes = (colours[colour])
