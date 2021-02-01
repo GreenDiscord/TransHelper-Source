c62b9f50b48e4765366c485251d27906fbdf08cc
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-01 23:18:26 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -7,7 +7,7 @@ class VotingMenu(menus.Menu):
 
     @menus.button('\N{WHITE HEAVY CHECK MARK}')
     async def on_check_mark(self, payload):
-        await self.message.edit(content=f'Thanks {self.ctx.author.mention}! Here's the [link](https://top.gg/bot/787820448913686539/vote)')
+        await self.message.edit(content=f"Thanks {self.ctx.author.mention}! Here's the [link](https://top.gg/bot/787820448913686539/vote)")
 
 
     @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
