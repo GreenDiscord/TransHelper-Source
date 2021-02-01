02ad9e6acbfb2bcc8ed1ed5f6f690466bc96d701
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-01 23:12:59 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -1,4 +1,5 @@
 from discord.ext import menus
+from asyncio import sleep as sl
 
 class VotingMenu(menus.Menu):
     async def send_initial_message(self, ctx, channel):
@@ -6,10 +7,12 @@ class VotingMenu(menus.Menu):
 
     @menus.button('\N{WHITE HEAVY CHECK MARK}')
     async def on_check_mark(self, payload):
-        await self.message.edit(content=f'Thanks {self.ctx.author.mention}! Here's the [link]()')
+        await self.message.edit(content=f'Thanks {self.ctx.author.mention}! Here's the [link](https://top.gg/bot/787820448913686539/vote)')
 
 
     @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
     async def on_stop(self, payload):
         self.stop()
+        await self.message.edit(content="Sorry to see you go!")
+        await sl(5)
         await self.message.delete()
