e5ef85c5c49f01d713c3357f3cb4c001c8173ea0
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-01 23:10:42 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -1,17 +1,15 @@
 from discord.ext import menus
 
-class MyMenu(menus.Menu):
+class VotingMenu(menus.Menu):
     async def send_initial_message(self, ctx, channel):
-        return await channel.send(f'Hello {ctx.author}')
+        return await channel.send(f'I see you would maybe like to vote {ctx.author.mention}, maybe react with your choice :)')
 
-    @menus.button('\N{THUMBS UP SIGN}')
-    async def on_thumbs_up(self, payload):
-        await self.message.edit(content=f'Thanks {self.ctx.author}!')
+    @menus.button('\N{WHITE HEAVY CHECK MARK}')
+    async def on_check_mark(self, payload):
+        await self.message.edit(content=f'Thanks {self.ctx.author.mention}! Here's the [link]()')
 
-    @menus.button('\N{THUMBS DOWN SIGN}')
-    async def on_thumbs_down(self, payload):
-        await self.message.edit(content=f"That's not nice {self.ctx.author}...")
 
     @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
     async def on_stop(self, payload):
         self.stop()
+        await self.message.delete()
