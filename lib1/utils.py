afe62c857a1a4a35c04877b3737a897f018c575c
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-01 22:34:06 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -1,10 +1,17 @@
-import discord
-from discord.ext import commands
+from discord.ext import menus
 
-class CustomContext(commands.Context):
-    async def tick(self, value):
-        emoji = '\N{WHITE HEAVY CHECK MARK}' if value else '\N{CROSS MARK}'
-        try:
-            await self.message.add_reaction(emoji)
-        except discord.HTTPException:
-            pass
+class MyMenu(menus.Menu):
+    async def send_initial_message(self, ctx, channel):
+        return await channel.send(f'Hello {ctx.author}')
+
+    @menus.button('\N{THUMBS UP SIGN}')
+    async def on_thumbs_up(self, payload):
+        await self.message.edit(content=f'Thanks {self.ctx.author}!')
+
+    @menus.button('\N{THUMBS DOWN SIGN}')
+    async def on_thumbs_down(self, payload):
+        await self.message.edit(content=f"That's not nice {self.ctx.author}...")
+
+    @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
+    async def on_stop(self, payload):
+        self.stop()
