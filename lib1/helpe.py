5833721261ab399281e3cd92509f8d678106f9fe
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-01 19:32:20 +0000

    Update helpe.py

--- lib1/helpe.py
+++ lib1/helpe.py
@@ -1,9 +1,18 @@
 import discord
+import asyncio
 from discord.ext import commands
 
-class NewHelp(commands.MinimalHelpCommand):
+class Help(commands.MinimalHelpCommand):
     async def send_pages(self):
         destination = self.get_destination()
         for page in self.paginator.pages:
             embed = discord.Embed(description=page)
-            await destination.send(embed=embed)
+            msg = await destination.send(embed=embed)
+            await asyncio.sleep(33)
+            await msg.delete()
+    
+    async def command_not_found(self, command):
+        destination = self.get_destination()
+        embed=discord.Embed(title="Help Error", description=f"Command '{command}' Not Found!")
+        await destination.send(embed=embed)
+        
