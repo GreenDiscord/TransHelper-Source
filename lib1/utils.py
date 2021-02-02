16821e557593baca551d7816d6d46efdf35f570c
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-02 00:14:08 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -11,7 +11,7 @@ class VotingMenu(menus.Menu):
     async def on_check_mark(self, payload):
         e1 = discord.Embed(title="Thanks!", description=f"Thanks {self.ctx.author.mention}! Here's the [link](https://top.gg/bot/787820448913686539/vote)")
         await self.message.edit(content="", embed=e1)
-        
+        await self.stop()
 
     @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
     async def on_stop(self, payload):
