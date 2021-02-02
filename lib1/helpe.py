b44f11952fe44f1e1c26205950a4e74c7ff5cffb
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-02 23:07:32 +0000

    Update helpe.py

--- lib1/helpe.py
+++ lib1/helpe.py
@@ -5,9 +5,11 @@ from discord.ext import commands
 class Help(commands.MinimalHelpCommand):
     async def send_pages(self):
         destination = self.get_destination()
+        ctx = self.context
+        channel = await ctx.bot.get_channel(destination.id)
         for page in self.paginator.pages:
             embed = discord.Embed(description=page)
-            msg = await destination.send(embed=embed)
+            msg = await channel.send(embed=embed)
             await asyncio.sleep(33)
             await msg.delete()
     
