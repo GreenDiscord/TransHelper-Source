98dc5a853a8a9a7589a5893936b5462a212668f6
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-02 23:20:07 +0000

    Update helpe.py

--- lib1/helpe.py
+++ lib1/helpe.py
@@ -4,15 +4,13 @@ from discord.ext import commands
 
 class Help(commands.MinimalHelpCommand):
     async def send_pages(self):
-        destination = self.get_destination()
         ctx = self.context
         mes = ctx.message
         for page in self.paginator.pages:
             embed = discord.Embed(description=page)
-            msg = await destination.send(embed=embed)
+            msg = await ctx.send(embed=embed)
             await mes.add_reaction("👍")
             await asyncio.sleep(33)
-            await mes.remove_reaction("👍", ctx.author)
             await msg.delete()
     
     async def command_not_found(self, command):
