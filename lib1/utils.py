9096662ebf289cf5f25bd7a9cba5403c97d9b9bb
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-02 17:08:39 +0000

    Update utils.py

--- lib1/utils.py
+++ lib1/utils.py
@@ -4,8 +4,9 @@ import traceback
 from asyncio import sleep as sl
 
 class VotingMenu(menus.Menu):
-    def __init__(self):
+    def __init__(self, bot):
         super().__init__(timeout=30.0, clear_reactions_after=True)
+        self.bot = bot
 
 
     async def send_initial_message(self, ctx, channel):
@@ -25,4 +26,29 @@ class VotingMenu(menus.Menu):
         await self.message.edit(content="", embed=e2)
         await sl(5)
         await self.message.delete()
+        
+class WhoMenu(menus.Menu):
+    def __init__(self):
+        super().__init__(timeout=30.0, clear_reactions_after=True)
+
+
+    async def send_initial_message(self, ctx, channel):
+        e = discord.Embed(title="I see you want to know more!", description=f"{ctx.author.mention}, click the checkmark for the Privacy Policy or the crossmark for just info!")
+        return await channel.send(embed=e)
+
+    @menus.button('\N{WHITE HEAVY CHECK MARK}')
+    async def on_add(self, payload):
+        e1 = discord.Embed(title="Well, Heres The Policy :)", description=f"We store this data for a couple reasons.\n Guild IDs are stored for commands like (prefix command, coming soon), so one servers prefix for that certain guild.\n Member IDs are stored for member specfic things, like th,createaccount thats meant to show the balance of that specific user.", color=discord.Colour.from_hsv(random.random(), 1, 1))
+        await self.message.edit(content="", embed=e1)
+        self.stop()
+
+    @menus.button('\N{NEGATIVE SQUARED CROSS MARK}')
+    async def on_stop(self, payload):
+        user = self.bot.get_user(787800565512929321)
+        e2 = discord.Embed(title="Hey!", description=f"Hi, I'm {self.bot.user}, I am developed by {user.name}, I have been running since the 10/12/20 :)", color=discord.Colour.from_hsv(random.random(), 1, 1))
+        self.stop()
+        await self.message.edit(content="", embed=e2)
+        await sl(5)
+        await self.message.delete()
+       
        
