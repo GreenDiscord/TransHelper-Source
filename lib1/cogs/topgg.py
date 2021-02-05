6938567e46265b8f336014a9bed0b26b127d5657
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-05 12:00:34 +0000

    Update topgg.py

--- lib1/cogs/topgg.py
+++ lib1/cogs/topgg.py
@@ -10,11 +10,11 @@ class TopGG(commands.Cog):
         self.bot = bot
         self.bot.topken = bot.topken
         self.dblpy = dbl.DBLClient(self.bot, self.bot.topken, autopost=True) 
-        self.c = self.bot.get_channel(800450802174263356)
 
     async def on_guild_post(self):
         print("Server count posted successfully")
-        await self.c.send(f"Updated Top.gg Server Stats, Current Guild Count {len(self.bot.guilds)}")
+        c = self.bot.get_channel(800450802174263356)
+        await c.send(f"Updated Top.gg Server Stats, Current Guild Count {len(self.bot.guilds)}")
 
 def setup(bot):
     bot.add_cog(TopGG(bot))
