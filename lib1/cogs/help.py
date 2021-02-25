5fc0ba81096e39a6ff1336d5b7c7a5357fb3a7ae
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-25 12:03:33 +0000

    Update help.py

--- lib1/cogs/help.py
+++ lib1/cogs/help.py
@@ -6,8 +6,21 @@ from discord.ext import commands
 class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    def mycheck():
+        def predicate(ctx):
+            if ctx.bot.maintenance == True:
+                if ctx.author.id == 787800565512929321:
+                    return commands.check(predicate)
+                else:
+                    await ctx.send("Commands are of due to maintance mode!")
+                    return False
+            else:
+                return True
+        return commands.check(predicate)
 
     @commands.command()
+    @mycheck()
     @commands.cooldown(1, 120, commands.BucketType.guild)
     async def support(self, ctx):
         channel = ctx.channel
