612fc26330661bbfb30bcc3ad013d1776748401e
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-25 12:17:43 +0000

    Update moderation.py

--- lib1/cogs/moderation.py
+++ lib1/cogs/moderation.py
@@ -45,7 +45,7 @@ class Moderation(commands.Cog):
                 if ctx.author.id == 787800565512929321:
                     return commands.check(predicate)
                 else:
-                    await ctx.send("Commands are of due to maintance mode!")
+                    ctx.send("Commands are of due to maintance mode!")
                     return False
             else:
                 return True
